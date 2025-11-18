6e1c45818b1ee08441a74dab201ba71eccfadb8e
Clint Lorenzo <clintlorenzo11@gmail.com>
2025-11-18 19:32:24 +0800

    Update gsap.js

--- js/gsap.js
+++ js/gsap.js
@@ -6,28 +6,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
     ease: "power1.inOut"
   });
 
-  const linkPopUp = gsap.timeline({defaults: {opacity: 0}});
-  linkPopUp.fromTo(
-    ".box-link1",
-    {duration: 0.2},
-    {opacity: 1, scale: 1, delay: 0} // starts at t = 3s
-  );
-  linkPopUp.fromTo(
-    ".box-link2",
-    {duration: 0.2},
-    {opacity: 1, scale: 1, delay: 0.1} // starts at t = 3s
-  );
-  linkPopUp.fromTo(
-    ".box-link3",
-    {duration: 0.2},
-    {opacity: 1, scale: 1, delay: 0.2} // starts at t = 3s
+  // const linkPopUp = gsap.timeline({defaults: {opacity: 0}});
+  // linkPopUp.fromTo(
+  //   ".box-link1",
+  //   {duration: 0.2},
+  //   {opacity: 1, scale: 1, delay: 0} 
+  // );
+  // linkPopUp.fromTo(
+  //   ".box-link2",
+  //   {duration: 0.2},
+  //   {opacity: 1, scale: 1, delay: 0.1} 
+  // );
+  // linkPopUp.fromTo(
+  //   ".box-link3",
+  //   {duration: 0.2},
+  //   {opacity: 1, scale: 1, delay: 0.2} 
     
-  );
-
-
-  // gsap.fromTo(
-  //   ".box-link",
-  //   {opacity: 0, duration: 1, x: -200},
-  //   {opacity: 1, x: 0} 
   // );
 });
