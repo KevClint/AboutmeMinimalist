1765dbb0487c420d093d0d1eb28eb9893a396713
Clint Lorenzo <clintlorenzo11@gmail.com>
2025-11-18 18:29:02 +0800

    addds achievements, better footer and where i live

--- js/map-api.js
+++ js/map-api.js
@@ -12,3 +12,29 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 // Add marker to your location
 L.marker([lat, lon]).addTo(map).bindPopup("My Location").openPopup();
+
+
+
+// clock
+function pad(n) {
+  return String(n).padStart(2, "0");
+}
+
+function updateClock() {
+  const now = new Date();
+
+  let hr = now.getHours();
+  let min = pad(now.getMinutes());
+  let sec = pad(now.getSeconds());
+
+  let ampm = hr >= 12 ? "PM" : "AM";
+
+  hr = hr % 12;
+  hr = hr ? hr : 12; // converts "0" to "12"
+  hr = pad(hr);
+
+  document.getElementById("clock").textContent = `${hr}:${min}:${sec} ${ampm}`;
+}
+
+setInterval(updateClock, 1000); // updates every second
+updateClock(); // run immediately
